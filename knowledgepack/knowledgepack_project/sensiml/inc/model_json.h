457b9f074c45033c1f3d64e314e03a842e2944c0
unknown <Vishwas-LT@Vishwasn-LT.india.tejasnetworks.com>
2021-06-01 22:39:40 +0530

    initial commit

---
+++ knowledgepack/knowledgepack_project/sensiml/inc/model_json.h
@@ -0,0 +1,8 @@
+#ifndef __MODEL_JSON_H__
+#define __MODEL_JSON_H__
+
+const char recognition_model_string_json[] = {"{\"NumModels\":1,\"ModelIndexes\":{\"0\":\"SAAAS_rank_4\"},\"ModelDescriptions\":[{\"Name\":\"SAAAS_rank_4\",\"ClassMaps\":{\"1\":\"Loosenedscrew_brokenarm\",\"2\":\"Normaloperation\",\"3\":\"brokenpropeller\",\"4\":\"obstructiontopropeller\",\"5\":\"stationary\",\"0\":\"Unknown\"},\"ModelType\":\"DecisionTreeEnsemble\"}]}"};
+
+int recognition_model_string_json_len = sizeof(recognition_model_string_json);
+
+#endif /* __MODEL_JSON_H__ */
